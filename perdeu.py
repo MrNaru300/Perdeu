468d0404f36ce3cf225e63f2ea2648867d3435fe
mrnaru300 <bruno.santi.oli@gmail.com>
2022-08-04 19:38:55 -0300

    [WIP] implementação de java

--- perdeu.py
+++ perdeu.py
@@ -42,22 +42,52 @@ from enum import Enum
 from pathlib import Path
 import re
 import os
-from typing import List
+from typing import List, Tuple
+from numpy import mat
 import rich
 import typer
 
-app = typer.Typer(epilog=__doc__, help="Pre-processa arquivos de C, C++ e Java para enviar para o Verde")
-precomile = typer.Typer()
+app = typer.Typer(short_help="Pre-processa arquivos de C, C++ e Java para enviar para o Verde")
 
 class SuportedLanguages(str, Enum):
-    Java = 'Java'
-    C = 'C'
-    Cpp = 'C++'
+    Java = 'java'
+    C = 'c'
+    Cpp = 'cpp'
 
 
+def _includeJava (fp: Path, dependecies_path: List[Path] = []) -> str:
+
+    if not fp.is_file():
+        raise FileNotFoundError(f'Arquivo {fp} não pode ser encontrado')
+
+    rich.print(":yellow_circle:", f"[yellow]Lendo arquivo: [bold purple]{fp}[/bold purple] ...[/yellow]")
+
+    saida = ""
 
 
+    with open(fp) as arquivo:
+
+        for dep in dependecies_path:
+            if dep.is_file():
+                deps = [dep]
+            elif dep.is_dir():
+                deps = [x for x in dep.iterdir()]
+            else:
+                raise FileNotFoundError(f'Arquivo ou pasta {fp} não pode ser encontrado')
         
+            for d in deps:
+                with open(d) as dep_file:
+                    saida += f"""
+//-----------------------Inicio da lib: {dep_file.name}-----------------------//
+{_includeJava(dep_file)}
+//-----------------------Fim da lib: {dep_file.name}-----------------------//"""
+    
+        for linha in arquivo:
+            saida += linha
+
+    return saida
+
+    
 def _includeC (fp: Path) -> str:
 
     if not fp.is_file():
@@ -93,12 +123,16 @@ def _includeC (fp: Path) -> str:
 
 
 @app.command(help="Pre-processa o arquivo adicionando as depencencias")
-def Precompilar (arquivo: Path,
+def Precompilar (
+    arquivo: Path,
     arquivo_saida: typer.FileTextWrite,
-    dependencias : List[Path] = [],
-    lang: SuportedLanguages = SuportedLanguages.C) -> None:
-        
+    dependencia : List[Path] = [],
+    lang: SuportedLanguages = typer.Option(default=None, show_default=False, help="Qual linguagem vai ser preprocessada (será considerada a extensão do arquivo se não especificado).")) -> None:
 
+    if lang == None:
+        lang = arquivo.name.split('.')[-1]
+
+    rich.print("[bold yellow]Tipo de arquivo:[/bold yellow]", f"[blue].{lang}[/blue]")
 
     rich.print("[bold green]Juntando arquivos...[/bold green]", ":page_facing_up:")
 
@@ -107,7 +141,7 @@ def Precompilar (arquivo: Path,
         case SuportedLanguages.C, SuportedLanguages.Cpp:
             arquivo_saida.write(_includeC(arquivo))
         case SuportedLanguages.Java:
-            raise NotImplementedError("Java ainda não implementado")
+            arquivo_saida.write(_includeJava(arquivo, dependecies_path=dependencia))
     
     
 
